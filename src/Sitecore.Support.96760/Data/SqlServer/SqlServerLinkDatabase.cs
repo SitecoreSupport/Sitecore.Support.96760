3a3bfbc869c60af03977a22724c7f1dd6d44e01f
DmytroBaranov <dbr@sitecore.net>
2019-09-05 19:04:51 +0300

    Fixing the issue.

---
+++ src/Sitecore.Support.96760/Data/SqlServer/SqlServerLinkDatabase.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Support.Data.SqlServer
+{
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Links;
+    public class SqlServerLinkDatabase : Sitecore.Data.SqlServer.SqlServerLinkDatabase
+    {
+        public SqlServerLinkDatabase(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void UpdateItemVersionReferences(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            ItemLink[] allLinks = item.Links.GetAllLinks();
+            this.UpdateItemVersionLinks(item, allLinks);
+        }
+    }
+}
