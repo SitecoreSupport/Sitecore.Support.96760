454d00237faa833f2d22afb0c571f770a73e456a
sc-ArtemKosohov <ark@sitecore.net>
2019-09-16 11:55:06 +0300

    Issue #1 resolved (code updated to match source implementation)

--- src/Sitecore.Support.96760/Data/SqlServer/SqlServerLinkDatabase.cs
+++ src/Sitecore.Support.96760/Data/SqlServer/SqlServerLinkDatabase.cs
@@ -3,6 +3,7 @@
     using Sitecore.Data.Items;
     using Sitecore.Diagnostics;
     using Sitecore.Links;
+    using System.Threading.Tasks;
     public class SqlServerLinkDatabase : Sitecore.Data.SqlServer.SqlServerLinkDatabase
     {
         public SqlServerLinkDatabase(string connectionString) : base(connectionString)
@@ -12,8 +13,11 @@
         public override void UpdateItemVersionReferences(Item item)
         {
             Assert.ArgumentNotNull(item, "item");
-            ItemLink[] allLinks = item.Links.GetAllLinks();
-            this.UpdateItemVersionLinks(item, allLinks);
+            Task task = Task.Factory.StartNew(delegate
+            {
+                ItemLink[] allLinks = item.Links.GetAllLinks();
+                this.UpdateItemVersionLinks(item, allLinks);
+            });
         }
     }
 }
